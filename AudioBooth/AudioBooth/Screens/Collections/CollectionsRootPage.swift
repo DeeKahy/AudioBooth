af3a22bbc47345b3c7bc261c17fa92870ed4a1a7
Jeremy Grenier <1220338+jeremygrenier@users.noreply.github.com>
2026-01-31 13:32:11 -0800

    Fix library tab and collection tab refresh on library id change

--- AudioBooth/AudioBooth/Screens/Collections/CollectionsRootPage.swift
+++ AudioBooth/AudioBooth/Screens/Collections/CollectionsRootPage.swift
@@ -18,56 +18,62 @@ struct CollectionsRootPage: View {
 
   @Binding var selectedType: CollectionType
 
+  var body: some View {
+    NavigationStack {
+      CollectionsRootContent(selectedType: $selectedType)
+        .id(libraries.current?.id)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+          ToolbarItem(placement: .principal) {
+            Picker("Collection Type", selection: $selectedType) {
+              Text("Series").tag(CollectionType.series)
+              Text("Collections").tag(CollectionType.collections)
+              Text("Playlists").tag(CollectionType.playlists)
+            }
+            .pickerStyle(.segmented)
+            .controlSize(.large)
+            .font(.subheadline)
+            .tint(.primary)
+          }
+        }
+        .navigationDestination(for: NavigationDestination.self) { destination in
+          switch destination {
+          case .book(let id):
+            BookDetailsView(model: BookDetailsViewModel(bookID: id))
+          case .playlist(let id):
+            CollectionDetailPage(model: CollectionDetailPageModel(collectionID: id, mode: .playlists))
+          case .collection(let id):
+            CollectionDetailPage(
+              model: CollectionDetailPageModel(collectionID: id, mode: .collections)
+            )
+          case .author(let id, let name):
+            AuthorDetailsView(model: AuthorDetailsViewModel(authorID: id, name: name))
+          case .series, .narrator, .genre, .tag, .offline, .authorLibrary:
+            LibraryPage(model: LibraryPageModel(destination: destination))
+
+          case .stats:
+            EmptyView()
+          }
+        }
+    }
+  }
+}
+
+private struct CollectionsRootContent: View {
+  @Binding var selectedType: CollectionsRootPage.CollectionType
+
   @StateObject private var series = SeriesPageModel()
   @StateObject private var collections = CollectionsPageModel(mode: .collections)
   @StateObject private var playlists = CollectionsPageModel(mode: .playlists)
 
   var body: some View {
-    NavigationStack {
-      VStack {
-        switch selectedType {
-        case .series:
-          SeriesPage(model: series)
-        case .collections:
-          CollectionsPage(model: collections)
-        case .playlists:
-          CollectionsPage(model: playlists)
-        }
-      }
-      .id(libraries.current?.id)
-      .navigationBarTitleDisplayMode(.inline)
-      .toolbar {
-        ToolbarItem(placement: .principal) {
-          Picker("Collection Type", selection: $selectedType) {
-            Text("Series").tag(CollectionType.series)
-            Text("Collections").tag(CollectionType.collections)
-            Text("Playlists").tag(CollectionType.playlists)
-          }
-          .pickerStyle(.segmented)
-          .controlSize(.large)
-          .font(.subheadline)
-          .tint(.primary)
-        }
-      }
-      .navigationDestination(for: NavigationDestination.self) { destination in
-        switch destination {
-        case .book(let id):
-          BookDetailsView(model: BookDetailsViewModel(bookID: id))
-        case .playlist(let id):
-          CollectionDetailPage(model: CollectionDetailPageModel(collectionID: id, mode: .playlists))
-        case .collection(let id):
-          CollectionDetailPage(
-            model: CollectionDetailPageModel(collectionID: id, mode: .collections)
-          )
-        case .author(let id, let name):
-          AuthorDetailsView(model: AuthorDetailsViewModel(authorID: id, name: name))
-        case .series, .narrator, .genre, .tag, .offline, .authorLibrary:
-          LibraryPage(model: LibraryPageModel(destination: destination))
-
-        case .stats:
-          EmptyView()
-        }
-      }
+    switch selectedType {
+    case .series:
+      SeriesPage(model: series)
+    case .collections:
+      CollectionsPage(model: collections)
+    case .playlists:
+      CollectionsPage(model: playlists)
     }
   }
 }
--- AudioBooth/AudioBooth/Screens/Library/LibraryRootPage.swift
+++ AudioBooth/AudioBooth/Screens/Library/LibraryRootPage.swift
@@ -17,51 +17,57 @@ struct LibraryRootPage: View {
   @Binding var selectedType: LibraryType
   @ObservedObject private var libraries = Audiobookshelf.shared.libraries
 
+  var body: some View {
+    NavigationStack {
+      LibraryRootContent(selectedType: $selectedType)
+        .id(libraries.current?.id)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+          ToolbarItem(placement: .principal) {
+            Picker("Library Type", selection: $selectedType) {
+              Text("Library").tag(LibraryType.library)
+              Text("Authors").tag(LibraryType.authors)
+              Text("Narrators").tag(LibraryType.narrators)
+            }
+            .pickerStyle(.segmented)
+            .controlSize(.large)
+            .font(.subheadline)
+            .tint(.primary)
+          }
+        }
+        .navigationDestination(for: NavigationDestination.self) { destination in
+          switch destination {
+          case .book(let id):
+            BookDetailsView(model: BookDetailsViewModel(bookID: id))
+          case .offline:
+            OfflineListView(model: OfflineListViewModel())
+          case .author(let id, let name):
+            AuthorDetailsView(model: AuthorDetailsViewModel(authorID: id, name: name))
+          case .series, .narrator, .genre, .tag, .authorLibrary:
+            LibraryPage(model: LibraryPageModel(destination: destination))
+          case .playlist, .collection, .stats:
+            EmptyView()
+          }
+        }
+    }
+  }
+}
+
+private struct LibraryRootContent: View {
+  @Binding var selectedType: LibraryRootPage.LibraryType
+
   @StateObject private var library = LibraryPageModel()
   @StateObject private var authors = AuthorsPageModel()
   @StateObject private var narrators = NarratorsPageModel()
 
   var body: some View {
-    NavigationStack {
-      VStack {
-        switch selectedType {
-        case .library:
-          LibraryPage(model: library)
-        case .authors:
-          AuthorsPage(model: authors)
-        case .narrators:
-          NarratorsPage(model: narrators)
-        }
-      }
-      .id(libraries.current?.id)
-      .navigationBarTitleDisplayMode(.inline)
-      .toolbar {
-        ToolbarItem(placement: .principal) {
-          Picker("Library Type", selection: $selectedType) {
-            Text("Library").tag(LibraryType.library)
-            Text("Authors").tag(LibraryType.authors)
-            Text("Narrators").tag(LibraryType.narrators)
-          }
-          .pickerStyle(.segmented)
-          .controlSize(.large)
-          .font(.subheadline)
-          .tint(.primary)
-        }
-      }
-      .navigationDestination(for: NavigationDestination.self) { destination in
-        switch destination {
-        case .book(let id):
-          BookDetailsView(model: BookDetailsViewModel(bookID: id))
-        case .offline:
-          OfflineListView(model: OfflineListViewModel())
-        case .author(let id, let name):
-          AuthorDetailsView(model: AuthorDetailsViewModel(authorID: id, name: name))
-        case .series, .narrator, .genre, .tag, .authorLibrary:
-          LibraryPage(model: LibraryPageModel(destination: destination))
-        case .playlist, .collection, .stats:
-          EmptyView()
-        }
-      }
+    switch selectedType {
+    case .library:
+      LibraryPage(model: library)
+    case .authors:
+      AuthorsPage(model: authors)
+    case .narrators:
+      NarratorsPage(model: narrators)
     }
   }
 }
